d72f3f08a462835c5cb3788008cc72a72ce2af36
Sergio E <ses110@gmail.com>
2020-12-09 13:37:12 -0600

    improvement: Convert User Widgets class as multiple StatefulWidgets that will represent the User screen

--- lib/userWidgets.dart
+++ lib/userWidgets.dart
@@ -1,36 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:yonomi_flutter_demo/graphql/basic_info.dart';
+import 'package:yonomi_flutter_demo/graphql/basic_info.graphql.dart';
 
+class tenantSectionWidget extends StatefulWidget {
+  final String tenantId;
+  final String tenantName;
 
-String getTenantId() {
-  return "1337";
-}
-
-final Center tenantWidget = Center(
-    child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Tenant Id: ", style: TextStyle(fontSize: 20)),
-          Text(getTenantId(), style: TextStyle(fontSize: 20)),
-        ])
-);
-
-final myController = TextEditingController();
-
-
-class UserIdWidget extends StatefulWidget {
+  const tenantSectionWidget({this.tenantId, this.tenantName});
 
   @override
-  _UserIdWidgetState createState() => _UserIdWidgetState();
-
+  _tenantSectionWidgetState createState() => _tenantSectionWidgetState();
 }
 
-class _UserIdWidgetState extends State<UserIdWidget> {
-  final userIdController = TextEditingController();
+class _tenantSectionWidgetState extends State<tenantSectionWidget> {
+  String tenantId = "";
+  String tenantName = "";
 
   @override
   void initState() {
+    tenantId = widget.tenantId;
+    tenantName = widget.tenantName;
     super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(child:
+                Text("Tenant Id: ", style: TextStyle(fontSize: 20))),
+              Expanded(child:
+                Text(tenantId, style: TextStyle(fontSize: 16))),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(child:
+                Text("Tenant Name: ", style: TextStyle(fontSize: 20))),
+              Expanded(child:
+                Text(tenantName, style: TextStyle(fontSize: 16))),
+            ]),
+      ],
+    );
+  }
+}
+
+
+class idSectionWidget extends StatefulWidget {
+  final String userId;
+
+  idSectionWidget({this.userId});
+
+  @override
+  _idSectionWidgetState createState() => _idSectionWidgetState();
+}
+
+class _idSectionWidgetState extends State<idSectionWidget> {
+  TextEditingController userIdController;
+
+  String userId;
+
+  @override
+  void initState() {
+    userId = widget.userId;
+    super.initState();
+    userIdController = new TextEditingController(text: userId);
 
     userIdController.addListener(_printLatestValue);
   }
@@ -55,6 +95,7 @@ class _UserIdWidgetState extends State<UserIdWidget> {
               Text("User Id: ", style: TextStyle(fontSize: 20))),
               Flexible(child:
               TextField(
+
                 controller: userIdController,
                 decoration: InputDecoration(
                     border: InputBorder.none,
@@ -68,7 +109,7 @@ class _UserIdWidgetState extends State<UserIdWidget> {
 
 }
 
-final Center userActivityWidget = Center(
+final Center activitySectionWidget = Center(
     child: Column(children: [
       Text("id"),
       Text("lastActivityAt"),
@@ -76,7 +117,49 @@ final Center userActivityWidget = Center(
     ],)
 );
 
-final Column userScreenWidget = Column(
-  mainAxisAlignment: MainAxisAlignment.spaceAround,
-  children: <Widget>[tenantWidget, UserIdWidget(), userActivityWidget],
-);
+class StatefulUserScreenWidget extends StatefulWidget {
+  @override
+  _StatefulUserScreenWidgetState createState() => _StatefulUserScreenWidgetState();
+}
+
+class _StatefulUserScreenWidgetState extends State<StatefulUserScreenWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Query(
+      options: QueryOptions(documentNode: BasicInfoQuery().document),
+      builder: (
+          QueryResult result, {
+            Refetch refetch,
+            FetchMore fetchMore
+          }) {
+            if(result.hasException) {
+              return Text(result.exception.toString());
+            }
+
+            if (result.loading && result.data == null) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            final tenantId = BasicInfo$Query.fromJson(result.data).tenant.id;
+            final tenantName = BasicInfo$Query.fromJson(result.data).tenant.displayName;
+
+            final userId = BasicInfo$Query.fromJson(result.data).me.id;
+            final userFirst = BasicInfo$Query.fromJson(result.data).me.firstActivityAt;
+            final userLast = BasicInfo$Query.fromJson(result.data).me.lastActivityAt;
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                tenantSectionWidget(tenantId: tenantId, tenantName: tenantName),
+                idSectionWidget(userId: userId),
+                activitySectionWidget
+              ],
+            );
+      },
+    );
+  }
+
+}
