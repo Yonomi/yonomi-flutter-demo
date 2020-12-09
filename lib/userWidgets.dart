bb3fdbda319f11bdaa46b07ca1e5446a8e564354
Sergio E <ses110@gmail.com>
2020-12-09 16:27:35 -0600

    refactor: Made tenantSection & activitySection Widgets to stateless Widgets

--- lib/userWidgets.dart
+++ lib/userWidgets.dart
@@ -4,17 +4,49 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:yonomi_flutter_demo/graphql/basic_info.dart';
 import 'package:yonomi_flutter_demo/graphql/basic_info.graphql.dart';
 
-class tenantSectionWidget extends StatefulWidget {
+class TenantSectionWidget extends StatelessWidget {
+  final String tenantId;
+  final String tenantName;
+  
+  TenantSectionWidget({this.tenantId, this.tenantName});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(child:
+              Text("Tenant Id: ", style: TextStyle(fontSize: 20))),
+              Expanded(child:
+              Text(tenantId, style: TextStyle(fontSize: 16))),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(child:
+              Text("Tenant Name: ", style: TextStyle(fontSize: 20))),
+              Expanded(child:
+              Text(tenantName, style: TextStyle(fontSize: 16))),
+            ]),
+      ],
+    );
+  }
+  
+}
+class StatefulTenantSectionWidget extends StatefulWidget {
   final String tenantId;
   final String tenantName;
 
-  const tenantSectionWidget({this.tenantId, this.tenantName});
+  const StatefulTenantSectionWidget({this.tenantId, this.tenantName});
 
   @override
-  _tenantSectionWidgetState createState() => _tenantSectionWidgetState();
+  _StatefulTenantSectionWidgetState createState() => _StatefulTenantSectionWidgetState();
 }
 
-class _tenantSectionWidgetState extends State<tenantSectionWidget> {
+class _StatefulTenantSectionWidgetState extends State<StatefulTenantSectionWidget> {
   String tenantId = "";
   String tenantName = "";
 
@@ -109,7 +141,7 @@ class _idSectionWidgetState extends State<idSectionWidget> {
 
 }
 
-class ActivitySectionWidget extends StatefulWidget {
+class ActivitySectionWidget extends StatelessWidget {
   final String userId;
   final DateTime userFirstActivity;
   final DateTime userLastActivity;
@@ -117,51 +149,33 @@ class ActivitySectionWidget extends StatefulWidget {
   const ActivitySectionWidget({this.userId, this.userFirstActivity, this.userLastActivity});
 
   @override
-  _ActivitySectionWidget createState() => _ActivitySectionWidget();
-}
-
-class _ActivitySectionWidget extends State<ActivitySectionWidget> {
-
-  String userId = "";
-  DateTime userFirstActivity;
-  DateTime userLastActivity;
-
-  @override
-  void initState() {
-    userId = widget.userId;
-    userFirstActivity = widget.userFirstActivity;
-    userLastActivity = widget.userLastActivity;
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Column(children: [
+      return Column(children: [
         Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Flexible(child:
-          Text("User Id: ", style: TextStyle(fontSize: 20))),
-          Expanded(child:
-          Text(userId, style: TextStyle(fontSize: 16))),
-        ]),
-      Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(child:
-            Text("User First Activity: ", style: TextStyle(fontSize: 20))),
-            Expanded(child:
-            Text(userLastActivity.toString(), style: TextStyle(fontSize: 16))),
-          ]),
-      Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(child:
-            Text("User Last Activity: ", style: TextStyle(fontSize: 20))),
-            Expanded(child:
-            Text(userFirstActivity.toString(), style: TextStyle(fontSize: 16))),
-          ]),
-    ],);
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(child:
+              Text("User Id: ", style: TextStyle(fontSize: 20))),
+              Expanded(child:
+              Text(userId, style: TextStyle(fontSize: 16))),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(child:
+              Text("User First Activity: ", style: TextStyle(fontSize: 20))),
+              Expanded(child:
+              Text(userLastActivity.toString(), style: TextStyle(fontSize: 16))),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(child:
+              Text("User Last Activity: ", style: TextStyle(fontSize: 20))),
+              Expanded(child:
+              Text(userFirstActivity.toString(), style: TextStyle(fontSize: 16))),
+            ]),
+      ],);
   }
 
 }
@@ -203,11 +217,13 @@ class _StatefulUserScreenWidgetState extends State<StatefulUserScreenWidget> {
             return Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                tenantSectionWidget(tenantId: tenantId, tenantName: tenantName),
+                TenantSectionWidget(tenantId: tenantId, tenantName: tenantName),
                 idSectionWidget(userId: userId),
-                ActivitySectionWidget(userId: userId,
+                ActivitySectionWidget(
+                  userId: userId,
                   userFirstActivity: userFirst,
-                  userLastActivity: userLast,)
+                  userLastActivity: userLast,
+                )
               ],
             );
       },
