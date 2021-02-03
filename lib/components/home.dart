c97187e91b7a15ee9b8cbe024d40b5c52c42265f
Sergio E <ses110@gmail.com>
2021-02-03 16:44:33 -0600

    refactor: in Home, remove user info widget

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:yonomi_flutter_demo/components/devices.dart';
 import 'package:yonomi_flutter_demo/themes/AppThemes.dart';
 
@@ -7,14 +6,11 @@ class HomeWidget extends StatelessWidget {
   static final String title = "Home";
 
   Widget build(BuildContext context) {
-    final Widget userQuery = getUserQueryWidget();
     final Widget deviceQuery = getDeviceQueryWidget();
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        SizedBox(height: 25),
-        userQuery,
         SizedBox(height: 25),
         deviceQuery,
         // Expanded(
@@ -23,57 +19,6 @@ class HomeWidget extends StatelessWidget {
         // ))
       ],
     );
-  }
-
-  Query getUserQueryWidget() {
-    final QueryOptions qo = QueryOptions(documentNode: gql(r'''
-      query basicInfo {
-        # "me" is the entry point to any data specific to the current user
-        me {
-          id
-          lastActivityAt
-          firstActivityAt
-        }
-      }'''));
-    return Query(
-        options: qo,
-        builder: (
-          QueryResult result, {
-          Future<QueryResult> Function() refetch,
-          FetchMore fetchMore,
-        }) {
-          if (result.hasException) {
-            return Text(result.exception.toString());
-          }
-
-          if (result.loading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              children: [
-                ListTile(
-                  tileColor: Colors.yellow,
-                  title: Text("Profile",
-                      style: const TextStyle(
-                        color: AppThemes.listViewTextColor,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20.0,
-                      )),
-                ),
-                SizedBox(height: 20),
-                Text(result.data['me']['id']),
-                Text(result.data['me']['lastActivityAt']),
-                Text(result.data['me']['firstActivityAt']),
-                SizedBox(
-                    height: 20) // Text(result.data['me']['firstActivityAt']),
-              ],
-            ),
-          );
-        });
   }
 
   Widget getDeviceQueryWidget() {
