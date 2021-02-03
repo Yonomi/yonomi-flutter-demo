79443175dd0d12b2448db33403d5348b326bf414
Sergio E <ses110@gmail.com>
2021-02-03 16:44:46 -0600

    refactor: In settings, add user Info widget

--- lib/components/accounts.dart
+++ lib/components/accounts.dart
@@ -6,6 +6,71 @@ class AccountsWidget extends StatelessWidget {
   static final String title = "Settings";
 
   Widget build(BuildContext context) {
+    final Widget userQuery = getUserQueryWidget();
+    final Widget accountsQuery = getAccountsQueryWidget();
+
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(height: 25),
+          userQuery,
+          SizedBox(height: 25),
+          accountsQuery,
+        ]);
+  }
+
+  Query getUserQueryWidget() {
+    final QueryOptions qo = QueryOptions(documentNode: gql(r'''
+      query basicInfo {
+        # "me" is the entry point to any data specific to the current user
+        me {
+          id
+          lastActivityAt
+          firstActivityAt
+        }
+      }'''));
+    return Query(
+        options: qo,
+        builder: (
+          QueryResult result, {
+          Future<QueryResult> Function() refetch,
+          FetchMore fetchMore,
+        }) {
+          if (result.hasException) {
+            return Text(result.exception.toString());
+          }
+
+          if (result.loading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              children: [
+                ListTile(
+                  tileColor: Colors.yellow,
+                  title: Text("Profile",
+                      style: const TextStyle(
+                        color: AppThemes.listViewTextColor,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20.0,
+                      )),
+                ),
+                SizedBox(height: 20),
+                Text(result.data['me']['id']),
+                Text(result.data['me']['lastActivityAt']),
+                Text(result.data['me']['firstActivityAt']),
+                SizedBox(
+                    height: 20) // Text(result.data['me']['firstActivityAt']),
+              ],
+            ),
+          );
+        });
+  }
+
+  Query getAccountsQueryWidget() {
     final QueryOptions qo = QueryOptions(documentNode: gql(r'''
       query linkedAccounts {
         me {
