d5ccd01e361c9aa6c0c8f2718acf2ad6872f16fa
Sergio E <ses110@gmail.com>
2021-03-15 08:41:17 -0600

    feat: template interface for generic Slider component

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,12 +1,11 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:yonomi_flutter_demo/assets/traits/name_icon_mapper.dart';
 import 'package:yonomi_flutter_demo/components/device_item_widget.dart';
 import 'package:yonomi_flutter_demo/providers/devices_provider.dart';
-import 'package:yonomi_flutter_demo/themes/app_themes.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:yonomi_flutter_demo/themes/color_constants.dart';
+import 'package:yonomi_flutter_demo/widgets/components/slider_component.dart';
 
 class HomeWidget extends StatelessWidget {
   static final String title = "Home";
@@ -17,6 +16,12 @@ class HomeWidget extends StatelessWidget {
   );
 
   Widget build(BuildContext context) {
+    return SliderComponent(
+      mode: SliderMode.singleSelection,
+      width: 150,
+      height: 150,
+      arcColor: ColorConstants.yonomiYellow,
+    );
     return Consumer<DevicesProvider>(
       builder: (context, data, child) {
         data.hydrateDevices();
