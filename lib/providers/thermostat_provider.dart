c44c868c602f7b661ba5335a03ff11d375f510d2
Suraj Adhikari <suraj.adhikari@yonomi.co>
2021-03-17 11:41:31 -0500

    Added provider method for thermostat mode

--- lib/providers/thermostat_provider.dart
+++ lib/providers/thermostat_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yonomi_flutter_demo/providers/request.dart';
-import 'package:yonomi_platform_sdk/repository/devices_repository.dart';
+import 'package:yonomi_platform_sdk/graphql/devices/thermostat/thermostat_queries.dart';
+import 'package:yonomi_platform_sdk/repository/devices/devices_repository.dart';
+import 'package:yonomi_platform_sdk/repository/devices/thermostat_repository.dart';
 
 class ThermostatProvider extends ChangeNotifier {
   ThermostatProvider(String deviceId) {
@@ -11,9 +13,11 @@ class ThermostatProvider extends ChangeNotifier {
   Device _deviceDetail;
 
   Future<void> setPointAction(String deviceId, double temperature) async {
-    print('temperature');
-    print(temperature);
-    DevicesRepository.setPointThermostat(request, deviceId, temperature);
+    ThermostatRepository.setPointThermostat(request, deviceId, temperature);
+  }
+
+  Future<void> setThermostatMode(String deviceId, ThermostatMode mode) async {
+    ThermostatRepository.setMode(request, deviceId, mode);
   }
 
   Future<Device> getDeviceDetail(String deviceId) async {
