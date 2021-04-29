9751e8bafd47025fe1470a88281969f0fd29a31b
Suraj Adhikari <suraj.adhikari@yonomi.co>
2021-04-29 17:11:39 -0500

    Fix test

--- test/ui/home_test.dart
+++ test/ui/home_test.dart
@@ -6,15 +6,20 @@ import 'package:yonomi_flutter_demo/components/device_item_widget.dart';
 import 'package:mockito/mockito.dart';
 import 'package:yonomi_flutter_demo/components/home.dart';
 import 'package:yonomi_flutter_demo/providers/devices_provider.dart';
+import 'package:yonomi_flutter_demo/providers/login_provider.dart';
 import 'package:yonomi_platform_sdk/repository/devices/devices_repository.dart';
 
 class MockDevicesProvider extends Mock implements DevicesProvider {}
 
+class MockLoginProvider extends Mock implements LoginProvider {}
+
 DevicesProvider mockProvider = MockDevicesProvider();
+// LoginProvider mockLoginProvider = MockLoginProvider();
 
 Widget createHomeWidget() {
   return MultiProvider(
     providers: [
+      Provider<LoginProvider>(create: (context) => MockLoginProvider()),
       ChangeNotifierProvider<DevicesProvider>.value(
         value: mockProvider,
       )
